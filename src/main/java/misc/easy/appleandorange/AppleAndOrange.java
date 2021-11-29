412ffd744893f8c8975e22fc823b178446d641de
rami11 <rami.serapian@gmail.com>
2021-11-29 18:55:40 -0500

    Add second solution for problem Apple and Orange

--- src/main/java/misc/easy/appleandorange/AppleAndOrange.java
+++ src/main/java/misc/easy/appleandorange/AppleAndOrange.java
@@ -1,14 +1,22 @@
 package misc.easy.appleandorange;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class AppleAndOrange {
 
     public static void main(String[] args) {
-        int[] apples = {-2, 2 ,1};
+        int[] apples = {-2, 2, 1};
         int[] oranges = {5, -6};
 
         countApplesAndOranges(7, 11, 5, 15, apples, oranges);
+
+        System.out.println();
+
+        List<Integer> appleList = List.of(-2, 2, 1);
+        List<Integer> orangeList = List.of(5, -6);
+
+        countApplesAndOranges2(7, 11, 5, 15, appleList, orangeList);
     }
 
     private static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
@@ -32,5 +40,21 @@ public class AppleAndOrange {
             }
         }
         Arrays.stream(result).forEach(System.out::println);
+    }
+
+    public static void countApplesAndOranges2(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+
+        int appleCount = (int) apples.stream()
+                .mapToInt(apple -> a + apple)
+                .filter(apple -> apple >= s && apple <= t)
+                .count();
+
+        int orangeCount = (int) oranges.stream()
+                .mapToInt(orange -> b + orange)
+                .filter(orange -> orange >= s && orange <= t)
+                .count();
+
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
     }
 }
