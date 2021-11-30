92adaf6f73baaa273d11413c7dd2698f6b2acb55
rami11 <rami.serapian@gmail.com>
2021-11-29 19:43:33 -0500

    Add a second solution for problem Birthday Cake Candles

--- src/main/java/misc/easy/birthdaycakecandles/BirthdayCakeCandles.java
+++ src/main/java/misc/easy/birthdaycakecandles/BirthdayCakeCandles.java
@@ -1,10 +1,17 @@
 package misc.easy.birthdaycakecandles;
 
+import java.util.List;
+
 public class BirthdayCakeCandles {
 
     public static void main(String[] args) {
         int[] candleLengths = {3, 2, 1, 3};
         System.out.println(birthdayCakeCandles(candleLengths));
+
+        System.out.println();
+
+        List<Integer> candleLengths2 = List.of(3, 2, 1, 3);
+        System.out.println(birthdayCakeCandles2(candleLengths2));
     }
 
     private static int birthdayCakeCandles(int[] ar) {
@@ -21,5 +28,19 @@ public class BirthdayCakeCandles {
             }
         }
         return candlesBlown;
+    }
+
+    public static int birthdayCakeCandles2(List<Integer> candles) {
+
+        int maxHeight = candles.stream()
+                .mapToInt(Integer::valueOf)
+                .max()
+                .orElse(-1);
+
+        return maxHeight < 0
+                ? 0
+                : (int) candles.stream().mapToInt(Integer::valueOf)
+                .filter(candle -> candle == maxHeight)
+                .count();
     }
 }
