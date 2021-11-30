0157fc566a4d407a5d1cdb692e60f94308c293ee
rami11 <rami.serapian@gmail.com>
2021-11-29 20:01:40 -0500

    Update solution for problem Breaking The Records

--- src/main/java/misc/easy/breakingtherecords/BreakingTheRecords.java
+++ src/main/java/misc/easy/breakingtherecords/BreakingTheRecords.java
@@ -1,5 +1,8 @@
 package misc.easy.breakingtherecords;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BreakingTheRecords {
 
     public static void main(String[] args) {
@@ -10,6 +13,12 @@ public class BreakingTheRecords {
 
         int[] scores2 = {3, 4, 21, 36, 10, 28, 35, 5, 24, 42};
         displayArray(breakingRecords(scores2));
+
+        List<Integer> scores11 = List.of(10, 5, 20, 20, 4, 5, 2, 25, 1);
+        System.out.println(breakingRecords2(scores11));
+
+        List<Integer> scores22 = List.of(3, 4, 21, 36, 10, 28, 35, 5, 24, 42);
+        System.out.println(breakingRecords2(scores22));
     }
 
     private static int[] breakingRecords(int[] scores) {
@@ -29,6 +38,31 @@ public class BreakingTheRecords {
         }
         return result;
     }
+
+    public static List<Integer> breakingRecords2(List<Integer> scores) {
+        List<Integer> output = new ArrayList<>();
+        if (scores == null || scores.isEmpty()) {
+            return output;
+        }
+        int maxScore = scores.get(0);
+        int minScore = scores.get(0);
+        int maxCount = 0;
+        int minCount = 0;
+        for (int i = 1; i < scores.size(); i++) {
+            int score = scores.get(i);
+            if (score < minScore) {
+                minScore = score;
+                minCount++;
+            } else if (score > maxScore) {
+                maxScore = score;
+                maxCount++;
+            }
+        }
+        output.add(maxCount);
+        output.add(minCount);
+        return output;
+    }
+
 
     private static void displayArray(int[] array) {
         for (int element : array) {
