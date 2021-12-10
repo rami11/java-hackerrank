d6451f4b278e8bf3a672897d6c6a4a0faf04d155
rami11 <rami.serapian@gmail.com>
2021-12-10 08:20:51 -0500

    Time conversion

---
+++ src/main/java/interview/prep/day1/timeconversion/TimeConversion.java
@@ -0,0 +1,26 @@
+package interview.prep.day1.timeconversion;
+
+public class TimeConversion {
+
+    public static void main(String[] args) {
+        String result = timeConversion("06:40:03AM");
+        System.out.println(result);
+    }
+
+    public static String timeConversion(String s) {
+
+        int hour = Integer.parseInt(s.substring(0, 2));
+        String amPm = s.substring(8);
+
+        String hh;
+        if (hour == 12) {
+            hh = String.format("%02d", amPm.equalsIgnoreCase("PM") ? hour : 0);
+        } else {
+            hh = String.format("%02d", amPm.equalsIgnoreCase("PM") ? hour + 12 : hour);
+        }
+
+        return hh + s.substring(2, 8);
+
+    }
+
+}
