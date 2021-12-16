14ed85b158ac7fd26fb2dde8e275a3f92223a4ea
rami11 <rami.serapian@gmail.com>
2021-12-16 18:03:40 -0500

    Super digit

---
+++ src/main/java/interview/prep/day4/superdigit/SuperDigit.java
@@ -0,0 +1,20 @@
+package interview.prep.day4.superdigit;
+
+import java.util.Arrays;
+
+public class SuperDigit {
+
+    public static void main(String[] args) {
+        System.out.println(superDigit2("9875", 4));
+    }
+
+    public static int superDigit2(String n, int k) {
+        if (n.length() == 1) {
+            return Integer.parseInt(n);
+        }
+
+        int sum = Arrays.stream(n.split("")).mapToInt(Integer::parseInt).sum();
+        String str = String.valueOf(sum);
+        return superDigit2(str.repeat(k), 1);
+    }
+}
