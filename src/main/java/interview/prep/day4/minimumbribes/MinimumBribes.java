b0e3713700d8b7fcf695a0c08c478c45221ef29d
rami11 <rami.serapian@gmail.com>
2021-12-17 20:44:04 -0500

    Minimum bribes (Naive)

---
+++ src/main/java/interview/prep/day4/minimumbribes/MinimumBribes.java
@@ -0,0 +1,31 @@
+package interview.prep.day4.minimumbribes;
+
+import java.util.List;
+
+public class MinimumBribes {
+
+    public static void minimumBribesNaive(List<Integer> q) {
+
+        int bribes = 0;
+        boolean tooChaotic = false;
+        for (int i = 0; i < q.size(); i++) {
+
+            int person = q.get(i);
+
+            int bribesToAdd = 0;
+            for (int j = i + 1; j < q.size(); j++) {
+                int prevPerson = q.get(j);
+                if (person > prevPerson) {
+                    bribesToAdd++;
+                }
+            }
+            if (bribesToAdd > 2) {
+                tooChaotic = true;
+                break;
+            }
+            bribes += bribesToAdd;
+        }
+
+        System.out.println(tooChaotic ? "Too chaotic" : bribes);
+    }
+}
