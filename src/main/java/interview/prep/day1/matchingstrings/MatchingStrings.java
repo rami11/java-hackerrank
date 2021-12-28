da57ad1c8cb036270afe5b0c2582b8bf4d19766e
rami11 <rami.serapian@gmail.com>
2021-12-27 19:10:21 -0500

    Matching strings

---
+++ src/main/java/interview/prep/day1/matchingstrings/MatchingStrings.java
@@ -0,0 +1,29 @@
+package interview.prep.day1.matchingstrings;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MatchingStrings {
+
+    public static void main(String[] args) {
+        System.out.println(matchingStrings(List.of("ab", "ab", "abc"), List.of("ab", "abc", "bc")));
+    }
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+
+        int[] output = new int[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String query = queries.get(i);
+
+            for (String string : strings) {
+                if (query.equals(string)) {
+                    output[i] += 1;
+                }
+            }
+
+        }
+
+        return Arrays.stream(output).boxed().collect(Collectors.toList());
+    }
+}
