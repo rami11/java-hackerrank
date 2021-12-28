6eb82e724ada6d40284ecece25de45387f2cd6a6
rami11 <rami.serapian@gmail.com>
2021-12-28 17:46:56 -0500

    Queue using two stacks (with jdk)

---
+++ src/main/java/interview/prep/day5/queueusingtwostacks/Main.java
@@ -0,0 +1,33 @@
+package interview.prep.day5.queueusingtwostacks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        while (q-- > 0) {
+
+            int type = scanner.nextInt();
+            int element = -1;
+            switch (type) {
+                case 1:
+                    element = scanner.nextInt();
+                    queue.addLast(element);
+                    break;
+                case 2:
+                    queue.removeFirst();
+                    break;
+                case 3:
+                    System.out.println(queue.peek());
+                    break;
+            }
+
+        }
+    }
+}
