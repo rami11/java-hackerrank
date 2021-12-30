865c39ce764033bc63f724703995b868191cd406
rami11 <rami.serapian@gmail.com>
2021-12-30 13:22:39 -0500

    Balanced brackets

---
+++ src/main/java/interview/prep/day5/balancedbrackets/BalancedBrackets.java
@@ -0,0 +1,49 @@
+package interview.prep.day5.balancedbrackets;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+
+public class BalancedBrackets {
+
+    public static void main(String[] args) {
+        String result = isBalanced("{{[[(())]]}}");
+        System.out.println(result);
+
+        result = isBalanced("{[(])}");
+        System.out.println(result);
+    }
+
+    public static String isBalanced(String s) {
+
+        if (s == null || s.isEmpty()) {
+            return "YES";
+        }
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        Map<Character, Character> bracketMap = Map.of(
+                '{', '}',
+                '(', ')',
+                '[', ']'
+        );
+
+        char[] brackets = s.toCharArray();
+        for (char bracket : brackets) {
+            if (bracketMap.containsKey(bracket)) {
+                stack.addLast(bracket);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                } else {
+                    char openingBracket = stack.removeLast();
+                    if (bracketMap.get(openingBracket) != bracket) {
+                        return "NO";
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+}
