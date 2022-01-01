ace0e275b2c1fc2984469ad9d001996c00838716
rami11 <rami.serapian@gmail.com>
2022-01-01 15:46:38 -0500

    Binary Tree: Preorder traversal

---
+++ src/main/java/interview/prep/day7/treepreordertraversal/TreeNode.java
@@ -0,0 +1,12 @@
+package interview.prep.day7.treepreordertraversal;
+
+public class TreeNode<T> {
+
+    public final T data;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    public TreeNode(T data) {
+        this.data = data;
+    }
+}
