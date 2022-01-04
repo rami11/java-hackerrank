3028664d88da78b6e0ac3d0c6c3a99e08c17f352
rami11 <rami.serapian@gmail.com>
2022-01-04 14:52:11 -0500

    Reverse doubly linked list

--- src/main/java/interview/prep/week3/reverseadoublylinkedlist/Main.java
+++ src/main/java/interview/prep/week3/reverseadoublylinkedlist/Main.java
@@ -1,29 +1,16 @@
-package interview.prep.week3.reversealinkedlist;
+package interview.prep.week3.reverseadoublylinkedlist;
 
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        Node<String> head = generateLinkedList(List.of("A", "C", "D"));
+        Node<Character> head = generateDoublyLinkedList(List.of('X', 'Y', 'Z'));
         displayLinkedList(head);
-
-        Node<String> reversed = reverseLinkedList(head);
-        displayLinkedList(reversed);
-        //---
-
-        System.out.println();
-
-        //--
-        Node<Integer> head2 = generateLinkedList(List.of(1, 5, 1, 2, 3, 4, 5));
-        displayLinkedList(head2);
-
-        Node<Integer> reversed2 = reverseLinkedList(head2);
-        displayLinkedList(reversed2);
     }
 
-    // x -> y -> z
-    private static <T> Node<T> reverseLinkedList(Node<T> head) {
+    public static <T> Node<T> reverse(Node<T> head) {
 
         if (head == null) {
             return null;
@@ -33,9 +20,9 @@ public class Main {
         Node<T> curr = head;
 
         while (curr != null) {
-
             Node<T> temp = curr.next;
             curr.next = prev;
+            curr.prev = temp;
 
             prev = curr;
             curr = temp;
@@ -44,7 +31,8 @@ public class Main {
         return prev;
     }
 
-    private static <T> Node<T> generateLinkedList(List<T> elements) {
+    // x y
+    private static <T> Node<T> generateDoublyLinkedList(List<T> elements) {
         Node<T> head = null;
         Node<T> tail = head;
         for (T element : elements) {
@@ -54,6 +42,7 @@ public class Main {
             if (head == null) {
                 head = node;
             } else {
+                node.prev = tail;
                 tail.next = node;
             }
             tail = node;
@@ -65,7 +54,7 @@ public class Main {
     private static <T> void displayLinkedList(Node<T> head) {
         Node<T> curr = head;
         while (curr.next != null) {
-            System.out.printf("%s -> ", curr.data);
+            System.out.printf("%s <-> ", curr.data);
             curr = curr.next;
         }
         System.out.println(curr.data);
